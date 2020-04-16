63139a21af2501971ffb4ada3a5249b6f2ea93f0
himasha23 <himashaamarasingha@gmail.com>
2020-04-17 04:48:16 +0530

    add,dlete patient details

--- healthcare/src/main/java/com/codebin/project/healthcare/PatientResource.java
+++ healthcare/src/main/java/com/codebin/project/healthcare/PatientResource.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -28,7 +31,7 @@ public class PatientResource {
 	
 	@POST
 	@Path("patient")
-	public Patient createnewPatient(Patient p) {
+	public Patient createnewPatient(Patient p) throws Exception {
 		mockup.createpatient(p);
 		return p;
 	}
@@ -43,4 +46,24 @@ public class PatientResource {
 	}
 	
 	*/
+	
+	
+	
+	@PUT
+	@Path("patient")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Patient updatePatient(Patient p) throws Exception  {
+		mockup.updatepatient(p);
+		return p;
+	}
+	
+	
+	@DELETE
+	@Path("patient/{name}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void dropPatient(@PathParam("name") String name) throws Exception  {
+		mockup.droppatient(name);
+	
+	}
+
 }
--- healthcare/src/main/java/com/codebin/project/healthcare/Patientmokup.java
+++ healthcare/src/main/java/com/codebin/project/healthcare/Patientmokup.java
@@ -2,6 +2,7 @@ package com.codebin.project.healthcare;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -57,11 +58,53 @@ public class Patientmokup {
 	
 	*/
 	
-	public void createpatient(Patient patient) {
+	public void createpatient(Patient patient) throws Exception{
 		
-		patients.add(patient);
+		 PreparedStatement ps = getConnection().prepareStatement("insert into patient (pid,pname,paddress,pmobile,page,pwd) values (?,?,?,?,?,?)");
+		ps.setString(1, patient.getPid());
+		ps.setString(2, patient.getPname());
+		ps.setString(3, patient.getPaddress());
+		ps.setString(4, patient.getPmobile());
+		ps.setString(5, patient.getPage());
+		ps.setString(6, patient.getPwd());
+		
+		ps.executeUpdate();
 		
 	}
+	
+	
+	public void updatepatient(Patient patient) throws Exception{
+		
+		 PreparedStatement ps = getConnection()
+				 .prepareStatement("update patient set pid=?,paddress=?,pmobile=?,page=?,pwd=? where pname=?");
+		ps.setString(1, patient.getPid());
+		ps.setString(2, patient.getPname());
+		ps.setString(3, patient.getPaddress());
+		ps.setString(4, patient.getPmobile());
+		ps.setString(5, patient.getPage());
+		ps.setString(6, patient.getPwd());
+		
+			
+		int count = ps.executeUpdate();
+			
+			System.out.println("Updated Count :"+count);
+			
+		}
+		
+		//System.out.println("Update Count :" +count +count +count +count +count +count);
+		
+		
+		public void droppatient(String name) throws Exception{
+			
+			 PreparedStatement ps = getConnection().prepareStatement("delete from patient where pname=?");
+			ps.setString(1, name);
+			
+			int count = ps.executeUpdate();
+			
+			System.out.println("Updated Count :"+count);
+	}
+	
+	
 	
 	
 public Connection getConnection() throws SQLException{
